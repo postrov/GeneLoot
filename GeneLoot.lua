1862177954112a340925268e03b1d3a0d3a84c7f
Pawel Ostrowski <postrov>
2014-02-11 17:17:03 +0100

    quotes unification, master looter check

--- GeneLoot.lua
+++ GeneLoot.lua
@@ -40,40 +40,40 @@ end
 
 --- loot spec -> officer mapping
 local LOOT_OFFICERS = {
-  str_dps = "Berrí",
-  str_tank = "Berrí",
-  agi_dps = "Aylje",
-  -- agi_tank = "Aylje",
-  int_dps = "Sorcelator",
-  int_healer = "Ktenn",
-  vanq_token = "Sorcelator",
-  conq_token = "Ktenn",
-  prot_token = "Aylje"
+  str_dps = 'Berrí',
+  str_tank = 'Berrí',
+  agi_dps = 'Aylje',
+  -- agi_tank = 'Aylje',
+  int_dps = 'Sorcelator',
+  int_healer = 'Ktenn',
+  vanq_token = 'Sorcelator',
+  conq_token = 'Ktenn',
+  prot_token = 'Aylje'
 }
 
 local function getLootSpec(item)
   local stats = item.itemStats
   local str, agi, int, spi, dodge, parry
-  str = stats["ITEM_MOD_STRENGTH_SHORT"] or 0
-  agi = stats["ITEM_MOD_AGILITY_SHORT"] or 0
-  int = stats["ITEM_MOD_INTELLECT_SHORT"] or 0
+  str = stats['ITEM_MOD_STRENGTH_SHORT'] or 0
+  agi = stats['ITEM_MOD_AGILITY_SHORT'] or 0
+  int = stats['ITEM_MOD_INTELLECT_SHORT'] or 0
   if str > 0 then
-    dodge = stats["ITEM_MOD_DODGE_RATING_SHORT"] or 0
-    parry = stats["ITEM_MOD_PARRY_RATING_SHORT"] or 0
-    return (parry > 0 or dodge > 0) and "str_tank" or "str_dps"
+    dodge = stats['ITEM_MOD_DODGE_RATING_SHORT'] or 0
+    parry = stats['ITEM_MOD_PARRY_RATING_SHORT'] or 0
+    return (parry > 0 or dodge > 0) and 'str_tank' or 'str_dps'
   elseif int > 0 then
-    spi = stats["ITEM_MOD_SPIRIT_SHORT"] or 0
-    return (spi > 0) and "int_healer" or "int_dps"
+    spi = stats['ITEM_MOD_SPIRIT_SHORT'] or 0
+    return (spi > 0) and 'int_healer' or 'int_dps'
   elseif agi > 0 then
-    return "agi_dps"
+    return 'agi_dps'
   else
     local name = item.itemName
-    if string.match(name, " Vanquisher$") then
-      return "vanq_token"
-    elseif string.match(name, " Protector$") then
-      return "prot_token"
-    elseif string.match(name, " Conqueror$") then
-      return "conq_token"
+    if string.match(name, ' Vanquisher$') then
+      return 'vanq_token'
+    elseif string.match(name, ' Protector$') then
+      return 'prot_token'
+    elseif string.match(name, ' Conqueror$') then
+      return 'conq_token'
     end
     -- todo: perhaps determine troublesome items by id
     return nil
@@ -108,10 +108,10 @@ local function announceLootItems(items)
   for i = 1, #items do 
       local item = items[i]
       local lootSpec = getLootSpec(item)
-      local lootOfficer = getLootOfficer(lootSpec) or "idk"
+      local lootOfficer = getLootOfficer(lootSpec) or 'idk'
       if item.itemRarity >= lootThreshold then
-        SendChatMessage(format("%d: %s -> %s", index, item.itemLink, lootOfficer), "RAID_WARNING")
-        -- print(format("%d: %s -> %s", index, item.itemLink, lootOfficer))
+        SendChatMessage(format('%d: %s -> %s', index, item.itemLink, lootOfficer), 'RAID_WARNING')
+        -- print(format('%d: %s -> %s', index, item.itemLink, lootOfficer))
         index = index + 1
       end
   end
@@ -120,10 +120,9 @@ end
 --- main function to be called when looting takes place
 local function processLoot()
   local numLootItems = GetNumLootItems()
-  local lootmethod, masterlooterPartyID, masterlooterRaidID = GetLootMethod()
-  -- todo: perhaps check if player is master looter
+
   if numLootItems == 0 then
-    print("Not looting now")
+    print('Not looting now')
   else
     local items = getLootItems(numLootItems)
     table.sort(items, function(a, b)
@@ -140,21 +139,33 @@ local function geneLootSlash(msg, editbox)
   processLoot()
 end
 
-SLASH_GENELOOT1 = "/geneloot"
+SLASH_GENELOOT1 = '/geneloot'
 SlashCmdList.GENELOOT = geneLootSlash
 
-local frame = CreateFrame("Frame", "GeneLootFrame")
-frame:RegisterEvent("LOOT_OPENED")
+local frame = CreateFrame('Frame', 'GeneLootFrame')
+frame:RegisterEvent('LOOT_OPENED')
+
+local function isPlayerMasterLooter()
+  if not IsInRaid() then
+    return false
+  end
+
+  local lootmethod, masterLooterPartyID, masterLooterRaidID = GetLootMethod()
+  if (lootmethod ~= 'master') then
+    return false;
+  end
+  
+  return UnitId(format('raid%d', masterLooterRaidID)) == UnitId('player')
+end
 
 local function eventHandler(self, event, ...) 
-  if event == "LOOT_OPENED" then
-    print("yay!")
-    if shouldAutoProcessLoot() then
+  if event == 'LOOT_OPENED' then
+    if isPlayerMasterLooter and shouldAutoProcessLoot() then
       resetLastLootInfo()
       processLoot()
     end
   end
 end
 
-frame:SetScript("OnEvent", eventHandler)
+frame:SetScript('OnEvent', eventHandler)
 
